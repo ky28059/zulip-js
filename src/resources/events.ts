970ca9561d527ede5520400fdcdff9cd145486e6
Kevin Yu <kevinyu567@gmail.com>
2026-02-02 16:07:55 -0500

    Preliminary `events` types

--- src/resources/events.ts
+++ src/resources/events.ts
@@ -1,17 +1,28 @@
 import type { ZulipRC } from '../zuliprc';
 import api from '../api';
 
-interface RetrieveEventsParams {
+export interface RetrieveEventsParams {
   queue_id: string,
   last_event_id?: number,
   dont_block?: boolean
+}
+
+export interface ZulipEvent {
+  id: number,
+  type: string,
+  [key: string]: any // TODO: add event types
+}
+
+export interface RetrieveEventsResponse {
+  events: ZulipEvent[],
+  queue_id: string
 }
 
 export default function events(config: ZulipRC) {
   return {
     retrieve: (params: RetrieveEventsParams) => {
       const url = `${config.apiURL}/events`;
-      return api(url, config, 'GET', params);
+      return api<RetrieveEventsResponse>(url, config, 'GET', params);
     },
   };
 }
