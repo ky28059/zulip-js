4c8b69ab8c12da1df7a994bdf611cb28870599cf
Kevin Yu <kevinyu567@gmail.com>
2026-02-02 16:22:40 -0500

    Add `typing` params type

--- src/resources/typing.ts
+++ src/resources/typing.ts
@@ -1,15 +1,19 @@
 import type { ZulipRC } from '../zuliprc';
 import api from '../api';
 
+export interface TypingParams {
+  type?: 'direct' | 'stream' | 'channel',
+  op: 'start' | 'stop',
+  to?: number[],
+  stream_id?: number,
+  topic?: string
+}
+
 export default function typing(config: ZulipRC) {
   return {
-    send: (initialParams: any) => {
+    send: (params: TypingParams) => {
       const url = `${config.apiURL}/typing`;
-      const params = { ...initialParams };
-      if (params.to.length > 1) {
-        params.to = JSON.stringify(params.to);
-      }
-      return api(url, config, 'POST', params);
+      return api<{}>(url, config, 'POST', params);
     },
   };
 }
