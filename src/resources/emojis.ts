bffd71c2d3f8a112781d02c6f2d05043288ebf68
Kevin Yu <kevinyu567@gmail.com>
2026-02-02 15:44:06 -0500

    Add `emojis` return type

--- src/resources/emojis.ts
+++ src/resources/emojis.ts
@@ -1,11 +1,28 @@
 import type { ZulipRC } from '../zuliprc';
 import api from '../api';
 
+export interface EmojiDetail {
+  id: string,
+  name: string,
+  source_url: string,
+  still_url?: string | null,
+  deactivated: boolean,
+  author_id?: number | null
+}
+
+export interface RetrieveEmojisResponse {
+  emoji: Record<string, EmojiDetail>,
+
+  // TODO
+  msg: string,
+  result: string,
+}
+
 export default function emojis(config: ZulipRC) {
   return {
-    retrieve: (params: any) => {
+    retrieve: (): Promise<RetrieveEmojisResponse> => {
       const url = `${config.apiURL}/realm/emoji`;
-      return api(url, config, 'GET', params);
+      return api(url, config, 'GET');
     },
   };
 }
