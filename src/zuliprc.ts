183e457233d2df4dc46889ff2e9fdb792140c74a
Kevin Yu <kevinyu567@gmail.com>
2026-02-02 14:59:20 -0500

    Add `ini` types, inline constant in `parseConfigFile`

--- src/zuliprc.ts
+++ src/zuliprc.ts
@@ -1,4 +1,4 @@
-import { promises as fsPromises } from 'fs';
+import { readFile } from 'fs/promises';
 import { parse } from 'ini';
 
 export interface ZulipRC {
@@ -9,13 +9,12 @@ export interface ZulipRC {
 }
 
 export default async function parseConfigFile(filename: string): Promise<ZulipRC> {
-  const data = await fsPromises.readFile(filename, 'utf8');
+  const data = await readFile(filename, 'utf8');
   const parsedConfig = parse(data);
-  const config: ZulipRC = {
+  return {
     realm: parsedConfig.api.site,
     username: parsedConfig.api.email,
     apiKey: parsedConfig.api.key,
     apiURL: `${parsedConfig.api.site}/api/v1`,
   };
-  return config;
 }
