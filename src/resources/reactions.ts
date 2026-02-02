7d546f061c34b3a06b6d02f32b5aa3cfb6eb859f
Kevin Yu <kevinyu567@gmail.com>
2026-02-02 15:27:22 -0500

    Add `reactions` param types

--- src/resources/reactions.ts
+++ src/resources/reactions.ts
@@ -1,18 +1,42 @@
 import type { ZulipRC } from '../zuliprc';
 import api from '../api';
 
+export interface AddReactionParams {
+  message_id: number,
+  emoji_name: string,
+  emoji_code?: string,
+  reaction_type?: 'unicode_emoji' | 'realm_emoji' | 'zulip_extra_emoji'
+}
+
+export interface RemoveReactionParams {
+  message_id: number,
+  emoji_name?: string,
+  emoji_code?: string,
+  reaction_type?: 'unicode_emoji' | 'realm_emoji' | 'zulip_extra_emoji'
+}
+
+// TODO: abstract this with `api`?
+export interface ReactionResponse {
+  result: string,
+  msg: string,
+  code?: string
+}
+
 export default function reactions(config: ZulipRC) {
-  const call = (method: 'POST' | 'DELETE', initParams: any) => {
+  const call = (
+    method: 'POST' | 'DELETE',
+    initParams: AddReactionParams | RemoveReactionParams,
+  ): Promise<ReactionResponse> => {
     const { message_id, ...params } = initParams;
     return api(
       `${config.apiURL}/messages/${message_id}/reactions`,
       config,
       method,
-      params
+      params,
     );
   };
   return {
-    add: (params: any) => call('POST', params),
-    remove: (params: any) => call('DELETE', params),
+    add: (params: AddReactionParams): Promise<ReactionResponse> => call('POST', params),
+    remove: (params: RemoveReactionParams): Promise<ReactionResponse> => call('DELETE', params),
   };
 }
